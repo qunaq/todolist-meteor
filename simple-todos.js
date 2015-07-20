dd10fc942e369b5d18ab5839eddfb78f700a42d3
Miya <miya@maggie.local>
2015-07-20 12:07:29 -0500

    login

--- simple-todos.js
+++ simple-todos.js
@@ -31,7 +31,9 @@ if (Meteor.isClient) {
       // Insert a task into the collection
       Tasks.insert({
         text: text,
-        createdAt: new Date() // current time
+        createdAt: new Date(),    // current time
+        owner: Meteor.userId(),   // _id of logged in user
+        username: Meteor.user().username // username of logged in user
       });
 
       // Clear form
@@ -52,5 +54,8 @@ if (Meteor.isClient) {
     "click .delete": function () {
       Tasks.remove(this._id);
     }
+  });
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
   });
 }
